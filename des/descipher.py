503f0ef9af72a21292ab001b320c163d5d980c8e
Leo <songhao.li@hotmail.com>
2023-02-10 21:55:40 -0600

    Updated

--- des/descipher.py
+++ des/descipher.py
@@ -9,16 +9,6 @@ from utils.matrixindexer import MatrixIndexer
 
 class DES:
     __sub_key: Dict[int, np.ndarray] = dict()
-    initial_permutation_matrix = np.array([
-        [58, 50, 42, 34, 26, 18, 10, 2],
-        [60, 52, 44, 36, 28, 20, 12, 4],
-        [62, 54, 46, 38, 30, 22, 14, 6],
-        [64, 56, 48, 40, 32, 24, 16, 8],
-        [57, 49, 41, 33, 25, 17, 9, 1],
-        [59, 51, 43, 35, 27, 19, 11, 3],
-        [61, 53, 45, 37, 29, 21, 13, 5],
-        [63, 55, 47, 39, 31, 23, 15, 7]
-    ])
 
     inverse_initial_permutation_matrix = np.array([
         [40, 8, 48, 16, 56, 24, 64, 32],
@@ -30,37 +20,6 @@ class DES:
         [33, 1, 41, 9, 49, 17, 57, 25]
     ])
 
-    expansion_matrix = np.array([
-        [32, 1, 2, 3, 4, 5],
-        [4, 5, 6, 7, 8, 9],
-        [8, 9, 10, 11, 12, 13],
-        [12, 13, 14, 15, 16, 17],
-        [16, 17, 18, 19, 20, 21],
-        [20, 21, 22, 23, 24, 25],
-        [24, 25, 26, 27, 28, 29],
-        [28, 29, 30, 31, 32, 1]
-    ])
-
-    PC1 = np.array([
-        [57, 49, 41, 33, 25, 17, 9],
-        [1, 58, 50, 42, 34, 26, 18],
-        [10, 2, 59, 51, 43, 35, 27],
-        [19, 11, 3, 60, 52, 44, 36],
-        [63, 55, 47, 39, 31, 23, 15],
-        [7, 62, 54, 46, 38, 30, 22],
-        [14, 6, 61, 53, 45, 37, 29],
-        [21, 13, 5, 28, 20, 12, 4]
-    ])
-
-    PC2 = np.array([
-        [14, 17, 11, 24, 1, 5, 3, 28],
-        [15, 6, 21, 10, 23, 19, 12, 4],
-        [26, 8, 16, 7, 27, 20, 13, 2],
-        [41, 52, 31, 37, 47, 55, 30, 40],
-        [51, 45, 33, 48, 44, 49, 39, 56],
-        [34, 53, 46, 42, 50, 36, 29, 32]
-    ])
-
     correct_PC2 = np.array([[14, 17, 11, 24, 1, 5],
                             [3, 28, 15, 6, 21, 10],
                             [23, 19, 12, 4, 26, 8],
@@ -70,64 +29,32 @@ class DES:
                             [44, 49, 39, 56, 34, 53],
                             [46, 42, 50, 36, 29, 32]])
 
-    S = {1: np.array([[1, 4, 4, 1, 3, 1, 2, 1, 5, 1, 1, 8, 3, 1, 0, 6, 1, 2, 5, 9, 0, 7],
-                      [0, 1, 5, 7, 4, 1, 4, 2, 1, 3, 1, 1, 0, 6, 1, 2, 1, 1, 9, 5, 3, 8],
-                      [4, 1, 1, 4, 8, 1, 3, 6, 2, 1, 1, 1, 5, 1, 2, 9, 7, 3, 1, 0, 5, 0],
-                      [1, 5, 1, 2, 8, 2, 4, 9, 1, 7, 5, 1, 1, 3, 1, 4, 1, 0, 0, 6, 1, 3]]),
-         2: np.array([[1, 5, 1, 8, 1, 4, 6, 1, 1, 3, 4, 9, 7, 2, 1, 3, 1, 2, 0, 5, 1, 0],
-                      [3, 1, 3, 4, 7, 1, 5, 2, 8, 1, 4, 1, 2, 0, 1, 1, 0, 6, 9, 1, 1, 5],
-                      [0, 1, 4, 7, 1, 1, 1, 0, 4, 1, 3, 1, 5, 8, 1, 2, 6, 9, 3, 2, 1, 5],
-                      [1, 3, 8, 1, 0, 1, 3, 1, 5, 4, 2, 1, 1, 6, 7, 1, 2, 0, 5, 1, 4, 9]]),
-         3: np.array([[1, 0, 0, 9, 1, 4, 6, 3, 1, 5, 5, 1, 1, 3, 1, 2, 7, 1, 1, 4, 2, 8],
-                      [1, 3, 7, 0, 9, 3, 4, 6, 1, 0, 2, 8, 5, 1, 4, 1, 2, 1, 1, 1, 5, 1],
-                      [1, 3, 6, 4, 9, 8, 1, 5, 3, 0, 1, 1, 1, 2, 1, 2, 5, 1, 0, 1, 4, 7],
-                      [1, 1, 0, 1, 3, 0, 6, 9, 8, 7, 4, 1, 5, 1, 4, 3, 1, 1, 5, 2, 1, 2]]),
-         4: np.array([[7, 1, 3, 1, 4, 3, 0, 6, 9, 1, 0, 1, 2, 8, 5, 1, 1, 1, 2, 4, 1, 5],
-                      [1, 3, 8, 1, 1, 5, 6, 1, 5, 0, 3, 4, 7, 2, 1, 2, 1, 1, 0, 1, 4, 9],
-                      [1, 0, 6, 9, 0, 1, 2, 1, 1, 7, 1, 3, 1, 5, 1, 3, 1, 4, 5, 2, 8, 4],
-                      [3, 1, 5, 0, 6, 1, 0, 1, 1, 3, 8, 9, 4, 5, 1, 1, 1, 2, 7, 2, 1, 4]]),
-         5: np.array([[2, 1, 2, 4, 1, 7, 1, 0, 1, 1, 6, 8, 5, 3, 1, 5, 1, 3, 0, 1, 4, 9],
-                      [1, 4, 1, 1, 2, 1, 2, 4, 7, 1, 3, 1, 5, 0, 1, 5, 1, 0, 3, 9, 8, 6],
-                      [4, 2, 1, 1, 1, 1, 0, 1, 3, 7, 8, 1, 5, 9, 1, 2, 5, 6, 3, 0, 1, 4],
-                      [1, 1, 8, 1, 2, 7, 1, 1, 4, 2, 1, 3, 6, 1, 5, 0, 9, 1, 0, 4, 5, 3]]),
-         6: np.array([[1, 2, 1, 1, 0, 1, 5, 9, 2, 6, 8, 0, 1, 3, 3, 4, 1, 4, 7, 5, 1, 1],
-                      [1, 0, 1, 5, 4, 2, 7, 1, 2, 9, 5, 6, 1, 1, 3, 1, 4, 0, 1, 1, 3, 8],
-                      [9, 1, 4, 1, 5, 5, 2, 8, 1, 2, 3, 7, 0, 4, 1, 0, 1, 1, 3, 1, 1, 6],
-                      [4, 3, 2, 1, 2, 9, 5, 1, 5, 1, 0, 1, 1, 1, 4, 1, 7, 6, 0, 8, 1, 3]]),
-         7: np.array([[4, 1, 1, 2, 1, 4, 1, 5, 0, 8, 1, 3, 3, 1, 2, 9, 7, 5, 1, 0, 6, 1],
-                      [1, 3, 0, 1, 1, 7, 4, 9, 1, 1, 0, 1, 4, 3, 5, 1, 2, 2, 1, 5, 8, 6],
-                      [1, 4, 1, 1, 1, 3, 1, 2, 3, 7, 1, 4, 1, 0, 1, 5, 6, 8, 0, 5, 9, 2],
-                      [6, 1, 1, 1, 3, 8, 1, 4, 1, 0, 7, 9, 5, 0, 1, 5, 1, 4, 2, 3, 1, 2]]),
-         8: np.array([[1, 3, 2, 8, 4, 6, 1, 5, 1, 1, 1, 1, 0, 9, 3, 1, 4, 5, 0, 1, 2, 7],
-                      [1, 1, 5, 1, 3, 8, 1, 0, 3, 7, 4, 1, 2, 5, 6, 1, 1, 0, 1, 4, 9, 2],
-                      [7, 1, 1, 4, 1, 9, 1, 2, 1, 4, 2, 0, 6, 1, 0, 1, 3, 1, 5, 3, 5, 8],
-                      [2, 1, 1, 4, 7, 4, 1, 0, 8, 1, 3, 1, 5, 1, 2, 9, 0, 3, 5, 6, 1, 1]])
-         }
-
-    def __init__(self, key: np.ndarray, size=64):
+    def __init__(self, key: np.ndarray, value: np.ndarray, size=64):
         self.__size = size
         self.__round = 1
-        self.__key_matrix = key
-        self.__value = self.__key_matrix
+        self.__key_matrix = key.flatten()
+        self.__value = value
+        self.__left = {}
+        self.__right = {}
         print(f'initial key is: \n{LatexConverter.convert_to_latex(self.__key_matrix)}')
 
     def generate_sub_key(self):
         print(f'round {self.__round}')
         if self.__round == 1:
-            self.__key_matrix = MatrixIndexer(self.PC1).convert(self.__key_matrix)
+            self.__key_matrix = PC1.apply(self.__key_matrix)
             print(f'do PC 1\n{LatexConverter.convert_to_latex(self.__key_matrix)}')
         self.__key_matrix = LeftShifter(self.__round).shift(self.__key_matrix)
 
         after_pc2 = MatrixIndexer(self.correct_PC2).convert(self.__key_matrix)
         print(f'do PC 2\n{LatexConverter.convert_to_latex(after_pc2)}')
-        self.__sub_key[self.__round] = after_pc2
+        self.__sub_key[self.__round] = after_pc2.flatten()
         return after_pc2
 
     class KeyMatrixBuilder:
         __key_matrix: np.ndarray
 
         def __init__(self, key, shape=None):
-            self.__key = key
+            self.__key = key.replace(' ', '')
             if shape is None:
                 key_len = len(self.__key)
                 width = int(math.sqrt(key_len))
@@ -141,66 +68,50 @@ class DES:
             self.__key_matrix = full_key_matrix
             return self.__key_matrix
 
-    def get_l_r_key(self):
-        return self.split_matrix(self.__key_matrix)
-
-    def get_l_r_value(self):
-        return self.split_matrix(self.__value)
-
     @staticmethod
     def split_matrix(matrix):
         return np.split(matrix, 2)
 
     def do_round(self):
-        if self.__round == 1:
-            self.__value = MatrixIndexer(self.initial_permutation_matrix).convert(self.__value)
-            print(f'IP(P_0)=\n{LatexConverter.convert_to_latex(self.__value)}')
-        current_round = self.__round - 1
-        left, right = self.get_l_r_value()
-        print(f'L_{current_round}=\n{LatexConverter.convert_to_latex(left)}')
-        print(f'R_{current_round}=\n{LatexConverter.convert_to_latex(right)}')
+        next_round = self.__round
+        current_round = next_round - 1
+        if next_round == 1:
+            self.__value = IP.apply(self.__value)
+            print(f'IP(P_0)=\n{self.__value}')
+            self.__left[current_round], self.__right[current_round] = np.split(self.__value, 2)
 
-        e_right = MatrixIndexer(self.expansion_matrix).convert(right)
+        print(f'L_{current_round}=\n{self.__left[current_round]}')
+        print(f'R_{current_round}=\n{self.__right[current_round]}')
+
+        self.calculate_next_left(current_round, next_round)
+
+        self.calculate_next_right(current_round, next_round)
+
+        return np.concatenate([self.__left[next_round], self.__right[next_round]])
+
+    def calculate_next_right(self, current_round, next_round):
+        e_right = Expansion.apply(self.__right[current_round])
         print(f'E[R_{current_round}]=\n{LatexConverter.convert_to_latex(e_right)}')
+        xor = np.logical_xor(e_right, self.__sub_key[next_round]).astype(int)
+        print(f'E[R_{current_round}]\\oplus K_{next_round}=\n{LatexConverter.convert_to_latex(xor)}')
+        after_s_substitute = SSub.apply(xor)
+        print(
+            f'S(E[R_{current_round}]\\oplus K_{next_round})=\n{LatexConverter.convert_to_latex(after_s_substitute)}')
+        f = PermutationP.apply(after_s_substitute)
+        print(f'f(R_{current_round}, K_{next_round}) = {LatexConverter.convert_to_latex(f)}')
+        self.__right[next_round] = XOR.apply(self.__left[current_round], f)
+        print(
+            f'''R_{next_round}=L_{current_round} \\oplus f(R_{current_round},K_{next_round}) =
+{LatexConverter.convert_to_latex(self.__left[current_round])} 
+            \\oplus 
+{LatexConverter.convert_to_latex(f)}''')
 
-        xor = np.logical_xor(e_right, self.__sub_key[self.__round]).astype(int)
-        print(f'E[R_{current_round}]\\oplus K_{self.__round}=\n{LatexConverter.convert_to_latex(xor)}')
-
-        after_s_substitute = self.substitute_s(xor)
+    def calculate_next_left(self, current_round, next_round):
+        self.__left[next_round] = self.__left[current_round]
+        print(f'''L_{next_round}=R_{current_round}={LatexConverter.convert_to_latex(self.__left[next_round])}''')
 
     def increment_round(self):
         self.__round += 1
-
-    def substitute_s(self, xor):
-        arrays = [np.split(xor, 8)][0]
-        result_matrices_buffer = []
-        for i in range(8):
-            six_bits = arrays[i]
-            print(six_bits)
-            print(f'row: {i + 1}. Six bits:\n{LatexConverter.convert_to_latex(six_bits)}')
-            row_index, col_index = self.get_substitution_indexes(six_bits)
-            current_s = self.S[i + 1]
-            value = current_s[row_index, col_index]
-            value_matrix = self.to_binary(value)
-            print(f'value is {value_matrix}')
-            result_matrices_buffer.append(value_matrix)
-        result_matrix = np.array(result_matrices_buffer)
-        print(f'S(E(R)\\oplusK=\n{LatexConverter.convert_to_latex(result_matrix)}')
-        return result_matrix
-
-    @staticmethod
-    def get_substitution_indexes(six_bits):
-        six_bits = six_bits.flatten()
-        col = sum(six_bits[1:5])
-        row = sum(six_bits) - col
-        return row, col
-
-    @staticmethod
-    def to_binary(value) -> List[int]:
-        no_padded = [int(x) for x in list('{0:0b}'.format(value))]
-        pad_to = 4
-        padded = [0] * (pad_to - len(no_padded)) + no_padded
-        return np.array(padded)
 
 
 class LeftShifter:
@@ -218,3 +129,158 @@ class LeftShifter:
         result = np.concatenate([shift_l, shift_r])
         print(f'left shift result:\n{LatexConverter.convert_to_latex(result.reshape(8, 7))}')
         return result
+
+
+class PC2:
+    matrix = np.array([
+        [14, 17, 11, 24, 1, 5, 3, 28],
+        [15, 6, 21, 10, 23, 19, 12, 4],
+        [26, 8, 16, 7, 27, 20, 13, 2],
+        [41, 52, 31, 37, 47, 55, 30, 40],
+        [51, 45, 33, 48, 44, 49, 39, 56],
+        [34, 53, 46, 42, 50, 36, 29, 32]
+    ])
+
+    @classmethod
+    def apply(cls, value):
+        return MatrixIndexer(cls.matrix).convert(value).flatten()
+
+
+class PC1:
+    matrix = np.array([
+        [57, 49, 41, 33, 25, 17, 9],
+        [1, 58, 50, 42, 34, 26, 18],
+        [10, 2, 59, 51, 43, 35, 27],
+        [19, 11, 3, 60, 52, 44, 36],
+        [63, 55, 47, 39, 31, 23, 15],
+        [7, 62, 54, 46, 38, 30, 22],
+        [14, 6, 61, 53, 45, 37, 29],
+        [21, 13, 5, 28, 20, 12, 4]
+    ])
+
+    @classmethod
+    def apply(cls, value):
+        return MatrixIndexer(cls.matrix).convert(value).flatten()
+
+
+class Expansion:
+    matrix = np.array([
+        [32, 1, 2, 3, 4, 5],
+        [4, 5, 6, 7, 8, 9],
+        [8, 9, 10, 11, 12, 13],
+        [12, 13, 14, 15, 16, 17],
+        [16, 17, 18, 19, 20, 21],
+        [20, 21, 22, 23, 24, 25],
+        [24, 25, 26, 27, 28, 29],
+        [28, 29, 30, 31, 32, 1]
+    ])
+
+    @classmethod
+    def apply(cls, value):
+        return MatrixIndexer(cls.matrix).convert(value).flatten()
+
+
+class XOR:
+    @staticmethod
+    def apply(value1, value2):
+        return np.logical_xor(value1, value2)
+
+
+class IP:
+    matrix = np.array([[58, 50, 42, 34, 26, 18, 10, 2],
+                       [60, 52, 44, 36, 28, 20, 12, 4],
+                       [62, 54, 46, 38, 30, 22, 14, 6],
+                       [64, 56, 48, 40, 32, 24, 16, 8],
+                       [57, 49, 41, 33, 25, 17, 9, 1],
+                       [59, 51, 43, 35, 27, 19, 11, 3],
+                       [61, 53, 45, 37, 29, 21, 13, 5],
+                       [63, 55, 47, 39, 31, 23, 15, 7]])
+
+    @classmethod
+    def apply(cls, value):
+        return MatrixIndexer(cls.matrix).convert(value).flatten()
+
+
+class SSub:
+    S = {1: np.array([[14, 4, 13, 1, 2, 15, 11, 8, 3, 10, 6, 12, 5, 9, 0, 7],
+                      [0, 15, 7, 4, 14, 2, 13, 1, 10, 6, 12, 11, 9, 5, 3, 8],
+                      [4, 1, 14, 8, 13, 6, 2, 11, 15, 12, 9, 7, 3, 10, 5, 0],
+                      [15, 12, 8, 2, 4, 9, 1, 7, 5, 11, 3, 14, 10, 0, 6, 13]]),
+         2: np.array([[15, 1, 8, 14, 6, 11, 3, 4, 9, 7, 2, 13, 12, 0, 5, 10],
+                      [3, 13, 4, 7, 15, 2, 8, 14, 12, 0, 1, 10, 6, 9, 11, 5],
+                      [0, 14, 7, 11, 10, 4, 13, 1, 5, 8, 12, 6, 9, 3, 2, 15],
+                      [13, 8, 10, 1, 3, 15, 4, 2, 11, 6, 7, 12, 0, 5, 14, 9]]),
+         3: np.array([[10, 0, 9, 14, 6, 3, 15, 5, 1, 13, 12, 7, 11, 4, 2, 8],
+                      [13, 7, 0, 9, 3, 4, 6, 10, 2, 8, 5, 14, 12, 11, 15, 1],
+                      [13, 6, 4, 9, 8, 15, 3, 0, 11, 1, 2, 12, 5, 10, 14, 7],
+                      [1, 10, 13, 0, 6, 9, 8, 7, 4, 15, 14, 3, 11, 5, 2, 12]]),
+         4: np.array([[7, 13, 14, 3, 0, 6, 9, 10, 1, 2, 8, 5, 11, 12, 4, 15],
+                      [13, 8, 11, 5, 6, 15, 0, 3, 4, 7, 2, 12, 1, 10, 14, 9],
+                      [10, 6, 9, 0, 12, 11, 7, 13, 15, 1, 3, 14, 5, 2, 8, 4],
+                      [3, 15, 0, 6, 10, 1, 13, 8, 9, 4, 5, 11, 12, 7, 2, 14]]),
+         5: np.array([[2, 12, 4, 1, 7, 10, 11, 6, 8, 5, 3, 15, 13, 0, 14, 9],
+                      [14, 11, 2, 12, 4, 7, 13, 1, 5, 0, 15, 10, 3, 9, 8, 6],
+                      [4, 2, 1, 11, 10, 13, 7, 8, 15, 9, 12, 5, 6, 3, 0, 14],
+                      [11, 8, 12, 7, 1, 14, 2, 13, 6, 15, 0, 9, 10, 4, 5, 3]]),
+         6: np.array([[12, 1, 10, 15, 9, 2, 6, 8, 0, 13, 3, 4, 14, 7, 5, 11],
+                      [10, 15, 4, 2, 7, 12, 9, 5, 6, 1, 13, 14, 0, 11, 3, 8],
+                      [9, 14, 15, 5, 2, 8, 12, 3, 7, 0, 4, 10, 1, 13, 11, 6],
+                      [4, 3, 2, 12, 9, 5, 15, 10, 11, 14, 1, 7, 6, 0, 8, 13]]),
+         7: np.array([[4, 11, 2, 14, 15, 0, 8, 13, 3, 12, 9, 7, 5, 10, 6, 1],
+                      [13, 0, 11, 7, 4, 9, 1, 10, 14, 3, 5, 12, 2, 15, 8, 6],
+                      [1, 4, 11, 13, 12, 3, 7, 14, 10, 15, 6, 8, 0, 5, 9, 2],
+                      [6, 11, 13, 8, 1, 4, 10, 7, 9, 5, 0, 15, 14, 2, 3, 12]]),
+         8: np.array([[13, 2, 8, 4, 6, 15, 11, 1, 10, 9, 3, 14, 5, 0, 12, 7],
+                      [1, 15, 13, 8, 10, 3, 7, 4, 12, 5, 6, 11, 0, 14, 9, 2],
+                      [7, 11, 4, 1, 9, 12, 14, 2, 0, 6, 10, 13, 15, 3, 5, 8],
+                      [2, 1, 14, 7, 4, 10, 8, 13, 15, 12, 9, 0, 3, 5, 6, 11]])
+         }
+
+    @classmethod
+    def apply(cls, xor):
+        arrays = np.split(xor, 8)
+        result_matrices_buffer = []
+        for i in range(8):
+            six_bits = arrays[i]
+            print(six_bits)
+            print(f'row: {i + 1}. Six bits:\n{LatexConverter.convert_to_latex(six_bits)}')
+            row_index, col_index = cls.get_substitution_indexes(six_bits)
+            current_s = cls.S[i + 1]
+            value = current_s[row_index, col_index]
+            print(f'value is {value}')
+            value_matrix = cls.to_binary(value)
+            print(f'value is {value_matrix}')
+            result_matrices_buffer.append(value_matrix)
+        result_matrix = np.array(result_matrices_buffer)
+        print(f'S(E(R)\\oplusK=\n{LatexConverter.convert_to_latex(result_matrix)}')
+        return result_matrix
+
+    @staticmethod
+    def get_substitution_indexes(six_bits):
+        six_bits = six_bits.flatten()
+        col = int(''.join(map(str, six_bits[1:5])), 2)
+        row = int(''.join(map(str, [six_bits[0], six_bits[-1]])), 2)
+        print(f'index: ({row}, {col})')
+        return row, col
+
+    @staticmethod
+    def to_binary(value) -> List[int]:
+        no_padded = [int(x) for x in list('{0:0b}'.format(value))]
+        pad_to = 4
+        padded = [0] * (pad_to - len(no_padded)) + no_padded
+        return np.array(padded)
+
+
+class PermutationP:
+    matrix = np.array([[16, 7, 20, 21],
+                       [29, 12, 28, 17],
+                       [1, 15, 23, 26],
+                       [5, 18, 31, 10],
+                       [2, 8, 24, 14],
+                       [32, 27, 3, 9],
+                       [19, 13, 30, 6],
+                       [22, 11, 4, 25]])
+
+    @classmethod
+    def apply(cls, value):
+        return MatrixIndexer(cls.matrix).convert(value).flatten()
--- des/test_descipher.py
+++ des/test_descipher.py
@@ -3,14 +3,16 @@ from unittest import TestCase
 import numpy as np
 from numpy import testing
 
-from descipher import DES, LeftShifter
+from descipher import DES, LeftShifter, PC2, PC1, Expansion, XOR, IP, SSub, PermutationP
+from utils.matrixindexer import MatrixIndexer
 
 
 class TestDES(TestCase):
     def setUp(self):
-        self.des = DES(self.key_matrix)
+        self.des = DES(self.key_matrix, self.value)
 
     key = '0000000100100011010001010110011110001001101010111100110111101111'
+    value = '0000000100100011010001010110011110001001101010111100110111101111'
     key_matrix = DES.KeyMatrixBuilder(key).build()
 
     def test_build_key_matrix(self):
@@ -30,20 +32,8 @@ class TestDES(TestCase):
                                [3, 2]])
         value_matrix = np.array([[1, 2, 3],
                                  [4, 5, 6]])
-        matrix_indexer = DES.MatrixIndexer(key_matrix)
+        matrix_indexer = MatrixIndexer(key_matrix)
         np.testing.assert_array_equal(matrix_indexer.convert(value_matrix), np.array([[6, 5], [3, 2]]))
-
-    def test_key_schedule(self):
-        np.testing.assert_array_equal(self.des.generate_sub_key(), np.array(
-            [[1., 0., 0., 1., 0., 0.],
-             [1., 0., 0., 1., 0., 1.],
-             [1., 0., 1., 0., 0., 0.],
-             [1., 1., 0., 0., 0., 1.],
-             [1., 1., 1., 0., 0., 0.],
-             [1., 1., 0., 0., 0., 0.],
-             [0., 0., 1., 1., 1., 0.],
-             [0., 0., 1., 0., 0., 0.]]
-        ))
 
     def test_np_split(self):
         array = np.array([[1, 2, 3],
@@ -64,20 +54,17 @@ class TestDES(TestCase):
 1 9 1 3 30 6 22 1 1 4 25'''
         print(f'''np.array([[{text}]])'''.replace(' ', ',').replace('\n', '],\n['))
 
-    def test_get_index(self):
-        array = np.array([[1, 2, 3, 4, 5, 6]])
-        print(DES.get_substitution_indexes(array))
-        np.testing.assert_array_equal(DES.get_substitution_indexes(array), (7, 14))
-
-    def test_binary(self):
-        np.testing.assert_array_equal(DES.to_binary(3), np.array([0, 0, 1, 1]))
-
-    def test_binary_2(self):
-        np.testing.assert_array_equal(DES.to_binary(1), np.array([0, 0, 0, 1]))
-
     def test_get_key(self):
-        key = '00010011 00110100 01010111 01111001 10011011 10111100 11011111 11110001'.replace(' ', '')
+        key = '00010011 00110100 01010111 01111001 10011011 10111100 11011111 11110001'
         print(DES.KeyMatrixBuilder(key).build())
+
+    def test_PC1(self):
+        value = '00010011 00110100 01010111 01111001 10011011 10111100 11011111 11110001'
+        value_matrix = DES.KeyMatrixBuilder(value, (1, 64)).build().flatten()
+        result = ' 1111000 0110011 0010101 0101111 0101010 1011001 1001111 0001111'
+        result_matrix = DES.KeyMatrixBuilder(result, (1, 56)).build().flatten()
+        actual = PC1().apply(value_matrix)
+        np.testing.assert_array_equal(actual, result_matrix)
 
     def test_left_shift(self):
         value = '11110000110011001010101011110101010101100110011110001111'
@@ -85,6 +72,14 @@ class TestDES(TestCase):
         result = '11100001100110010101010111111010101011001100111100011110'
         result_matrix = DES.KeyMatrixBuilder(result, (1, 56)).build().flatten()
         actual = LeftShifter(1).shift(value_matrix)
+        np.testing.assert_array_equal(actual, result_matrix)
+
+    def test_PC2(self):
+        value = '11100001100110010101010111111010101011001100111100011110'
+        value_matrix = DES.KeyMatrixBuilder(value, (1, 56)).build().flatten()
+        result = '000110110000001011101111111111000111000001110010'.replace(' ', '')
+        result_matrix = DES.KeyMatrixBuilder(result, (1, 48)).build().flatten()
+        actual = PC2.apply(value_matrix)
         np.testing.assert_array_equal(actual, result_matrix)
 
     def test_example(self):
@@ -96,7 +91,9 @@ class TestDES(TestCase):
                         [1, 0, 1, 1, 1, 1, 0, 0],
                         [1, 1, 0, 1, 1, 1, 1, 1],
                         [1, 1, 1, 1, 0, 0, 0, 1]])
-        des = DES(key)
+        value = '0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111'
+        value_matrix = DES.KeyMatrixBuilder(value).build().flatten()
+        des = DES(key, value_matrix)
         sub_key = des.generate_sub_key()
         np.testing.assert_array_equal(sub_key, np.array([[0, 0, 0, 1, 1, 0],
                                                          [1, 1, 0, 0, 0, 0],
@@ -106,14 +103,78 @@ class TestDES(TestCase):
                                                          [0, 0, 0, 1, 1, 1],
                                                          [0, 0, 0, 0, 0, 1],
                                                          [1, 1, 0, 0, 1, 0]]))
+        ciphertext = des.do_round()
+        expected_ciphertext = '1111 0000 1010 1010 1111 0000 1010 10101110 1111 0100 1010 0110 0101 0100 0100'
+        expected_ciphertext_matrix = DES.KeyMatrixBuilder(expected_ciphertext, (1, 64)).build().flatten()
+        np.testing.assert_array_equal(ciphertext, expected_ciphertext_matrix)
+
+    def test_expansion(self):
+        value = '1111 0000 1010 1010 1111 0000 1010 1010'
+        value_matrix = DES.KeyMatrixBuilder(value, (1, 32)).build().flatten()
+        result = ' 011110 100001 010101 010101 011110 100001 010101 010101'.replace(' ', '')
+        result_matrix = DES.KeyMatrixBuilder(result, (1, 48)).build().flatten()
+        actual = Expansion.apply(value_matrix)
+        np.testing.assert_array_equal(actual, result_matrix)
+
+    def test_XOR(self):
+        value1 = ' 000110 110000 001011 101111 111111 000111 000001 110010'
+        value_matrix1 = DES.KeyMatrixBuilder(value1, (1, 48)).build().flatten()
+        value2 = '011110 100001 010101 010101 011110 100001 010101 010101'
+        value_matrix2 = DES.KeyMatrixBuilder(value2, (1, 48)).build().flatten()
+        result = ' 011000 010001 011110 111010 100001 100110 010100 100111'.replace(' ', '')
+        result_matrix = DES.KeyMatrixBuilder(result, (1, 48)).build().flatten()
+        actual = XOR.apply(value_matrix1, value_matrix2)
+        np.testing.assert_array_equal(actual, result_matrix)
 
     def test_transform(self):
-        key = '''14    17   11    24     1    5
-                  3    28   15     6    21   10
-                 23    19   12     4    26    8
-                 16     7   27    20    13    2
-                 41    52   31    37    47   55
-                 30    40   51    45    33   48
-                 44    49   39    56    34   53
-                 46    42   50    36    29   32'''
-        print(repr(np.array(key.rsplit()).reshape((8, 6)).astype(int)))
+        key = '''
+                    16   7  20  21
+                         29  12  28  17
+                          1  15  23  26
+                          5  18  31  10
+                          2   8  24  14
+                         32  27   3   9
+                         19  13  30   6
+                         22  11   4  25
+'''
+        print(repr(np.array(key.rsplit()).reshape((8, 4)).astype(int)))
+
+    def test_IP(self):
+        value = '0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111'
+        value_matrix = DES.KeyMatrixBuilder(value, (1, 64)).build().flatten()
+        result = '1100 1100 0000 0000 1100 1100 1111 1111 1111 0000 1010 1010 1111 0000 1010 1010'.replace(' ', '')
+        result_matrix = DES.KeyMatrixBuilder(result, (1, 64)).build().flatten()
+        actual = IP.apply(value_matrix)
+        np.testing.assert_array_equal(actual, result_matrix)
+
+    def test_split(self):
+        value = '1100 1100 0000 0000 1100 1100 1111 1111 1111 0000 1010 1010 1111 0000 1010 1010'
+        value_matrix = DES.KeyMatrixBuilder(value, (1, 64)).build().flatten()
+        result = '1100 1100 0000 0000 1100 1100 1111 1111'
+        result_matrix = DES.KeyMatrixBuilder(result, (1, 32)).build().flatten()
+        actual = np.split(value_matrix, 2)[0]
+        np.testing.assert_array_equal(actual, result_matrix)
+
+    def test_split_r(self):
+        value = '1100 1100 0000 0000 1100 1100 1111 1111 1111 0000 1010 1010 1111 0000 1010 1010'
+        value_matrix = DES.KeyMatrixBuilder(value, (1, 64)).build().flatten()
+        result = '1111 0000 1010 1010 1111 0000 1010 1010'
+        result_matrix = DES.KeyMatrixBuilder(result, (1, 32)).build().flatten()
+        actual = np.split(value_matrix, 2)[1]
+        np.testing.assert_array_equal(actual, result_matrix)
+
+    def test_S(self):
+        value = ' 011000 010001 011110 111010 100001 100110 010100 100111'
+        value_matrix = DES.KeyMatrixBuilder(value, (1, 48)).build().flatten()
+        result = ' 0101 1100 1000 0010 1011 0101 1001 0111'
+        result_matrix = DES.KeyMatrixBuilder(result, (1, 32)).build().flatten()
+        actual = SSub.apply(value_matrix).flatten()
+        np.testing.assert_array_equal(actual, result_matrix)
+
+    def test_permutation(self):
+        value = '0101 1100 1000 0010 1011 0101 1001 0111'
+        value_matrix = DES.KeyMatrixBuilder(value, (1, 32)).build().flatten()
+        result = '0010 0011 0100 1010 1010 1001 1011 1011'
+        result_matrix = DES.KeyMatrixBuilder(result, (1, 32)).build().flatten()
+        actual = PermutationP.apply(value_matrix).flatten()
+        np.testing.assert_array_equal(actual, result_matrix)
